0b1ed54daefd6fdef5fc5547b2335822f78697f1
Erik Sabin <eriksabin97@gmail.com>
2022-10-18 23:30:17 -0400

    part 1 finished

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+require('dotenv').config()
 const express = require('express')
 
 const app = express()
+
+app.use(`/places`, require(`./places/places`))
 
 app.get('/', (req, res) => {
 res.send('Hello World')
 })
 
-app.listen(process.env.port)
+app.get(`*`, (req, res) => {
+res.status(404) .send('<h1>Error page not found</h1>')
+})
+
+app.listen(process.env.PORT)
